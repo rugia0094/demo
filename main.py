c874c702d52231f7cd693986dda1ae75556be771
rugia0094 <rugia0094@yandex.ru>
2023-05-17 14:21:32 +0300

    make script to read text from file

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from words2numsrus import NumberExtractor
 
-def main():
-    extractor = NumberExtractor()
 
-    print(extractor.replace_groups('сто одиннадцать тысяч роз'))
+def main():
+    with open("text.txt", "r", encoding="utf-8") as file:
+        text = file.read()
+        print(NumberExtractor().replace_groups(text))
 
 
 if __name__ == "__main__":
